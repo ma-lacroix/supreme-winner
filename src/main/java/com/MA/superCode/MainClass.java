a49049037f06435a9bc7ef0676410b7afc2fcf5f
mlacroix <marc.lacroix@booking.com>
2023-03-19 22:53:46 +0100

    Section 1 stuff

---
+++ src/main/java/com/MA/superCode/MainClass.java
@@ -0,0 +1,12 @@
+package com.MA.superCode;
+
+import com.MA.superCode.resources.SomeUtils;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+
+        SomeUtils someUtils = new SomeUtils("MA");
+        someUtils.printInitials();
+    }
+}
---
+++ src/main/java/com/MA/superCode/resources/SomeUtils.java
@@ -0,0 +1,54 @@
+package com.MA.superCode.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SomeUtils {
+    // Section 1 exercises
+
+    public String userName;
+
+    public SomeUtils() {
+        this.userName = "Default";
+    }
+
+    public SomeUtils(String userName) {
+        this.userName = userName;
+    }
+
+    public void printInitials() {
+        List<String> data = drawInitials();
+        for (String line: data) {
+            System.out.println(line);
+        }
+    }
+
+    public List<String> drawInitials() {
+        List<String> stuffToPrint = new ArrayList<>();
+        stuffToPrint.add("");
+        stuffToPrint.add("");
+        stuffToPrint.add("");
+        stuffToPrint.add("");
+        stuffToPrint.add("");
+        for (char letter: userName.toCharArray()) {
+            if (letter == 'M') {
+                stuffToPrint.set(0, stuffToPrint.get(0) + "M    M");
+                stuffToPrint.set(1, stuffToPrint.get(1) + "MM  MM");
+                stuffToPrint.set(2, stuffToPrint.get(2) + "M MM M");
+                stuffToPrint.set(3, stuffToPrint.get(3) + "M    M");
+                stuffToPrint.set(4, stuffToPrint.get(4) + "M    M");
+            }
+            else if (letter == 'A') {
+                stuffToPrint.set(0, stuffToPrint.get(0) + "  AA  ");
+                stuffToPrint.set(1, stuffToPrint.get(1) + " A  A ");
+                stuffToPrint.set(2, stuffToPrint.get(2) + "A AA A");
+                stuffToPrint.set(3, stuffToPrint.get(3) + "A    A");
+                stuffToPrint.set(4, stuffToPrint.get(4) + "A    A");
+            } else {
+                System.out.println("Sorry too lazy to build other letters");
+            }
+        }
+        return stuffToPrint;
+    }
+
+}
